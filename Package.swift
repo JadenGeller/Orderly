dc4aa5249b92724f057a443c36fe148f15ba9988
Jaden Geller <jaden.geller@gmail.com>
2017-03-28 18:37:20 -0700

    Fixed structure

--- Package.swift
+++ Package.swift
@@ -2,8 +2,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Orderly",
-    dependencies: [
-        .Package(url: "https://github.com/JadenGeller/Comparator.git", majorVersion: 2)
-    ]
+    name: "Orderly"
 )
--- Templates/compile.sh
+++ Templates/compile.sh
@@ -4,12 +4,15 @@ if [ ! -e gyb.py ]; then
     curl -L --remote-name https://github.com/apple/swift/raw/master/utils/gyb.py
 fi
 
-echo 'Generating `Orderly.swift`...'
+
+echo 'Generating...'
+
 python gyb.py --line-directive '' -o Orderly.swift.split Orderly.swift.split.gyb
 
 python split.py Orderly.swift.split
 
 mv *.swift ../Sources/Orderly
 rm Orderly.swift.split
+
 
 echo 'Done!'
